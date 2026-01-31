1ecbeb45b498cdd101fb229bb613d152611cc451
Deivison Johnny <deivisonjohnny@gmail.com>
2026-01-31 18:24:36 -0300

    prisma client

--- src/config/prisma-client.ts
+++ src/config/prisma-client.ts
@@ -1,11 +1,29 @@
-import 'dotenv/config'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from 'generated/prisma/client'
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client'; // <--- AQUI ERA O ERRO (agora está correto)
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    // Cria o Pool de conexão usando o driver nativo 'pg'
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
-const connectionString = `${process.env.DATABASE_URL}`
+    // Conecta o Prisma ao driver do Postgres
+    const adapter = new PrismaPg(pool);
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+    // Inicializa o Prisma Client usando o adapter
+    super({ adapter });
+  }
 
-export default prisma
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
