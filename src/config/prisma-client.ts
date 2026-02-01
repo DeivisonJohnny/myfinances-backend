f02b5ce47e8d568a325a3ad934ff764181848086
Deivison Johnny <deivisonjohnny@gmail.com>
2026-01-31 21:30:55 -0300

    chore: Downgrade Prisma to version 6, removing driver adapter configuration and `prisma.config.ts`.

--- src/config/prisma-client.ts
+++ src/config/prisma-client.ts
@@ -1,24 +1,11 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client'; // <--- AQUI ERA O ERRO (agora está correto)
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  constructor() {
-    // Cria o Pool de conexão usando o driver nativo 'pg'
-    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-
-    // Conecta o Prisma ao driver do Postgres
-    const adapter = new PrismaPg(pool);
-
-    // Inicializa o Prisma Client usando o adapter
-    super({ adapter });
-  }
-
   async onModuleInit() {
     await this.$connect();
   }
